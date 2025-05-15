f75f7cc200e4c79f8cf897687d2c6baf535427dd
Henry <elegant.craft@hotmail.com>
2025-05-15 14:42:58 +0800

    env

--- Sources/WatchCore/LogManager.swift
+++ Sources/WatchCore/LogManager.swift
@@ -15,14 +15,18 @@ public struct LogManager {
     }
     
     public static func info(_ message: Any, file: String = #file, line: Int = #line) {
+        #if DEBUG
         let fileName = (file as NSString).lastPathComponent
         let formattedDate = dateFormatter.string(from: Date())
         print("🟩 INFO      \(padString(fileName, toLength: 30)):\(padString(String(line), toLength: 4)) \(formattedDate) ► \(message)")
+        #endif
     }
     
     static func lifecyle(_ message: Any, file: String = #file, line: Int = #line) {
+        #if DEBUG
         let fileName = (file as NSString).lastPathComponent
         let formattedDate = dateFormatter.string(from: Date())
         print("♻️ LIFECYCLE =================================   \(formattedDate) ► \(message)")
+        #endif
     }
 }
